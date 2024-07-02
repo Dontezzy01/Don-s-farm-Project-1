e6b096977945e8195993b5636bdcc22872dced1b
Ace <you@example.com>
2024-07-02 20:56:41 +0100

    'fix-service-section'

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const section1 = document.querySelector('#home');
 const nav = document.querySelector('.nav');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
@@ -334,7 +334,7 @@ const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
-document.getElementById('section--1');
+document.getElementById('home');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
